0814e107d7a7f85965cb3450f7b8b2f159adda29
hchiam <hchiam@gmail.com>
2020-02-27 00:24:42 -0500

    video playback control

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 if (window.CHARLIE) {
   window.CHARLIE.setup(document.getElementById('video'));
 }
+
+/** to be used by event listener */
+function playVideo() {
+  const video = document.getElementById('video');
+  video.play();
+}
+
+/** to be used by event listener */
+function pauseVideo() {
+  const video = document.getElementById('video');
+  video.pause();
+}
+
+document.getElementById('play-video').addEventListener('click', () => {
+  playVideo();
+});
+
+document.getElementById('pause-video').addEventListener('click', () => {
+  pauseVideo();
+});
