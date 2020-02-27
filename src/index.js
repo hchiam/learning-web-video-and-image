35a2d9b757be8997b09fa30ed71dcace6b51a0e0
hchiam <hchiam@gmail.com>
2020-02-26 22:27:03 -0500

    charlie.js connecting video and css animation

--- src/index.js
+++ src/index.js
@@ -1,31 +1,3 @@
-/* eslint-disable require-jsdoc */
-
-const solution = (a, b) => {
-  return a + b;
-};
-
-function solutionWrapper(...parameters) {
-  return solution(...parameters);
+if (window.CHARLIE) {
+  window.CHARLIE.setup(document.getElementById('video'));
 }
-
-// function ListNode(val) {
-//   this.val = val;
-//   this.next = null;
-// }
-
-// function TreeNode(val) {
-//   this.val = val;
-//   this.left = this.right = null;
-// }
-
-if (typeof module !== 'undefined') {
-  module.exports = {
-    solutionWrapper,
-  };
-}
-
-/**
- * This file was first created using the Yeoman generator
- * generator-hchiam-learning:
- * https://www.npmjs.com/package/generator-hchiam-learning
- */
